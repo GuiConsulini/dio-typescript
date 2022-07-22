1236138a1a9bdde800085c19abf0b036bae45381
GuiConsulini <guilherme.consulini1@gmail.com>
2022-07-22 13:54:23 -0300

    Desafio typescript

---
+++ desafio01.ts
@@ -0,0 +1,10 @@
+/*Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {};
+employee.code = 10;
+employee.name = "John";*/
+
+const funcionario = {
+    codigo: 10,
+    nome: 'João'
+};
